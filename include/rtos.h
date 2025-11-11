2ee1a1ee9bb643683d69834fc37e34c403b269bc
akshit-a9 <akshitsharma2002@gmail.com>
2025-11-11 14:14:02 +0530

    Priority Scheduling and round robin added for testing on fpga

--- include/rtos.h
+++ include/rtos.h
@@ -1,12 +1,32 @@
-// rtos.h
+// // rtos.h
+// #ifndef RTOS_H
+// #define RTOS_H
+
+// #include "task.h"
+// #include "scheduler.h"
+
+// void RTOS_Init(void);
+// void RTOS_CreateTask(void (*taskFunc)(void *), void *param, int priority);
+// void RTOS_Start(void);
+
+// #endif
+
 #ifndef RTOS_H
 #define RTOS_H
 
 #include "task.h"
 #include "scheduler.h"
 
+// Timeslice for round-robin (in microseconds)
+#ifndef TIMESLICE_US
+#define TIMESLICE_US 100000   // 100 ms
+#endif
+
 void RTOS_Init(void);
 void RTOS_CreateTask(void (*taskFunc)(void *), void *param, int priority);
 void RTOS_Start(void);
+
+/* Exposed only for unit testing; not required by user code */
+void *RTOS_Tick(void *arg);
 
 #endif
